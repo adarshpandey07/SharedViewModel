3df8a8e1265e9b97d3d42feff68b2e9bfcac2f2f
a23135306 <a_adarsh.pandey@africa.airtel.com>
2023-01-05 15:58:31 +0530

    fin

---
+++ app/src/main/java/com/example/sharedviewmodel/MessageSenderFragment.kt
@@ -0,0 +1,43 @@
+package com.example.sharedviewmodel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+
+
+class MessageSenderFragment : Fragment() {
+
+    lateinit var btn: Button
+
+    // to write message
+    lateinit var writeMSg: EditText
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_message_sender, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // reference for button and EditText
+        btn = view.findViewById(R.id.button)
+        writeMSg = view.findViewById(R.id.writeMessage)
+
+        // create object of SharedViewModel
+        val model = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+
+        // call function "sendMessage" defined in SharedVieModel
+        // to store the value in message.
+        btn.setOnClickListener { model.sendMessage(writeMSg.text.toString()) }
+    }
+
+}
